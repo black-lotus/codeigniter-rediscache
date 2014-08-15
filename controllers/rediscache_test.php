e416ac852f31ca8d7c767fc320c906f2ed121818
black lotus <donnydiunindra@gmail.com>
2014-08-15 10:46:19 +0700

    Update rediscache_test.php
    more simple to using redis cache
    $this->load->library('credis');
    //set cache
    $this->credis->set('keys_or_name', 'your_value', $expired);
    //get cache
    $this->credis->get('keys_or_name');
    //delete cache
    $this->credis->del('keys_or_name');
    
    Thanks

--- controllers/rediscache_test.php
+++ controllers/rediscache_test.php
@@ -10,24 +10,24 @@ class Rediscache_test extends CI_Controller {
 	public function index () {
 		$this->load->library('unit_test');
 		$this->load->library('redis');
-		$this->load->library('rediscache');
+		$this->load->library('credis');
 		$this->unit->use_strict(TRUE);
 
 		// string
 		$testVar = 'testvar';
-		$this->unit->run($this->rediscache->set('rc.key', $testVar, 10000), true, 'set string');
-		$this->unit->run($this->rediscache->get('rc.key'), $testVar, 'get string');
-		var_dump($this->rediscache->get('rc.key'));
-		$this->unit->run($this->rediscache->del('rc.key'), true, 'del string');
+		$this->unit->run($this->credis->set('rc.key', $testVar, 10000), true, 'set string');
+		$this->unit->run($this->credis->get('rc.key'), $testVar, 'get string');
+		var_dump($this->credis->get('rc.key'));
+		$this->unit->run($this->credis->del('rc.key'), true, 'del string');
 
 		// array
 		$testArr = array(
 			'hoge' => 'foo',
 			'fuga' => 'bar'
 		); 
-		$this->unit->run($this->rediscache->set('rc.key', $testArr, 10000), true, 'set array');
-		$this->unit->run($this->rediscache->get('rc.key'), $testArr, 'get array');
-		$this->unit->run($this->rediscache->del('rc.key'), true, 'del array');
+		$this->unit->run($this->credis->set('rc.key', $testArr, 10000), true, 'set array');
+		$this->unit->run($this->credis->get('rc.key'), $testArr, 'get array');
+		$this->unit->run($this->credis->del('rc.key'), true, 'del array');
 
 		// nested array with space
 		$testArr = array(
@@ -36,30 +36,52 @@ class Rediscache_test extends CI_Controller {
 				'foo bar key 2' => 'foo bar value 2'
 			)
 		); 
-		$this->unit->run($this->rediscache->set('rc.key', $testArr, 10000), true, 'set nested array with space');
-		$this->unit->run($this->rediscache->get('rc.key'), $testArr, 'get nest array with space');
-		$this->unit->run($this->rediscache->del('rc.key'), true, 'del array');
+		$this->unit->run($this->credis->set('rc.key', $testArr, 10000), true, 'set nested array with space');
+		$this->unit->run($this->credis->get('rc.key'), $testArr, 'get nest array with space');
+		$this->unit->run($this->credis->del('rc.key'), true, 'del array');
 
 
 		// object
 		$testObj = new Testobj;
-		$this->unit->run($this->rediscache->set('rc.key', $testObj, 10000), true, 'set object');
-		$this->unit->run($this->rediscache->get('rc.key')->var, $testObj->var, 'get object');
-		$this->unit->run($this->rediscache->del('rc.key'), true, 'del object');
+		$this->unit->run($this->credis->set('rc.key', $testObj, 10000), true, 'set object');
+		$this->unit->run($this->credis->get('rc.key')->var, $testObj->var, 'get object');
+		$this->unit->run($this->credis->del('rc.key'), true, 'del object');
 
 		// function (should be false)
 		$testFunc = function(){};
-		$this->unit->run($this->rediscache->set('rc.key', $testFunc, 10000), false, 'set function (should be false)');
-		$this->unit->run($this->rediscache->get('rc.key'), false, 'get function (should be false)');
+		$this->unit->run($this->credis->set('rc.key', $testFunc, 10000), false, 'set function (should be false)');
+		$this->unit->run($this->credis->get('rc.key'), false, 'get function (should be false)');
 
 		// resource (should be false)
 		$testRes = curl_init();
-		$this->unit->run($this->rediscache->set('rc.key', $testRes, 10000), false, 'set resource (should be false)');
-		$this->unit->run($this->rediscache->get('rc.key'), false, 'get resource (should be false)');
+		$this->unit->run($this->credis->set('rc.key', $testRes, 10000), false, 'set resource (should be false)');
+		$this->unit->run($this->credis->get('rc.key'), false, 'get resource (should be false)');
 		curl_close($testRes);
 
 		echo $this->unit->report();
 
+	}
+	
+	public function credis()
+	{
+		$arr = array(
+			'1' => 'satu',
+			'2' => 'dua',
+			'3' => 'tiga',
+			'4' => 'empat',
+			'5' => 'lima'
+		);
+		$redis = $this->credis->get('rc.test');
+		if(!$redis){
+			$this->credis->set('rc.test', $arr);
+			$redis = $arr;
+		}
+		var_dump($redis);
+	}
+	
+	public function getKeys()
+	{
+		var_dump($this->credis->getKeys());
 	}
 
 }
